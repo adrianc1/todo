e22c142a14f1d280716352c6217d7e893cc6e98d
Adrian <adriancrosby02@gmail.com>
2024-04-16 00:35:00 -0700

    created starting files and installed webpack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './style.css';
+import addIcon from './addicon.svg';
+
+const myIcon = new Image();
+myIcon.src = addIcon;
+/* myIcon.createAttribute('id', 'icon') */
+
+document.body.appendChild(myIcon)
+
+
+
+console.log('shiiiiit')
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};

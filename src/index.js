39f13c2d9618019a24b9f8795a666ed4b6e5ba19
Adrian <adriancrosby02@gmail.com>
2024-04-19 00:10:01 -0700

    created the first line. just need to clean up the code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 import './style.css';
 import addIcon from './addicon.svg';
 
+
+/* need to finish building out module class for this app */
+class taskManager {
+    constructor(mainContent) {
+        this.mainContent = mainContent;
+    }
+    addTaskContainer() {
+        const addTaskContainer = document.createElement('div');
+        addTaskContainer.setAttribute('id', 'task-container');
+        return addTaskContainer;
+    }
+};
+
+/* Create HTML Elements */
+
+
+const addTaskText = document.createElement('span');
+
+addTaskText.innerHTML = 'Add Task here';
+
+/* create add icon */
 const myIcon = new Image();
 myIcon.src = addIcon;
-/* myIcon.createAttribute('id', 'icon') */
 
-document.body.appendChild(myIcon)
+/* Set element attributes */
+myIcon.setAttribute('id', 'icon');
+addTaskText.setAttribute('id',  'task-text');
 
 
+/* Append containers to main content */
+addTaskContainer.appendChild(myIcon);
+addTaskContainer.appendChild(addTaskText);
+mainContent.appendChild(addTaskContainer)
 
-console.log('shiiiiit')
+
+/* Populate input field on clicking text */
+addTaskText.addEventListener('click', inputTask);
+
+function inputTask() {
+    const input = document.createElement('input');
+    addTaskText.classList.add('hidden');
+
+    addTaskContainer.appendChild(input);
+}

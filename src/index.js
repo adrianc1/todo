669376b516b4076fcb04b3868ff884312d8464fd
Adrian <adriancrosby02@gmail.com>
2024-04-23 23:45:37 -0700

    reverted back to old ProjectContainer

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import addIcon from './addicon.svg';
 import svg from './trash.svg'
 import createForm from './form.js';
 const header = document.getElementById('heading');
-const mainContent = document.getElementById("main-content");
+
 class TaskManager {
-/*     constructor() {
+    constructor() {
         this.mainContent = document.getElementById("main-content");
-    } */
+    }
+
+    taskItem() {
+        const taskItem = document.createElement('li');
+        taskItem.setAttribute('class', 'task-item');
+        return taskItem;
+    }
 
     addIcon() {
         const myIcon = new Image();
@@ -16,28 +22,42 @@ class TaskManager {
         return myIcon;
     }
 
-    static trashIcon() {
+    trashIcon() {
         const trash = new Image();
         trash.src = svg;
         trash.setAttribute('class', 'delete');
         return trash;
     }
 
+    appendElement(taskContainer) {
+        const taskItem = this.taskItem();
+        const deletebtn = this.trashIcon();
+        const addTaskText = "Add Task Here";
+        taskItem.contentEditable = 'true';
+        taskItem.append(addTaskText);
+        taskItem.append(deletebtn)
+        taskContainer.append(taskItem);
+        deletebtn.addEventListener('click', () => {
+            taskItem.remove();
+        })
+        this.mainContent.appendChild(taskContainer);
+        return this;
+    }
+
     createNewTask() {
         const newTask = new TaskManager();
-        newTask.appendTask(taskContainer.taskContainer);
+        newTask.appendElement(taskContainer.taskContainer);
         header.addEventListener('click', newTask.createNewTask);
         return this;
     }
 
     deleteTask() {
-       const deletebtn = TaskManager.trashIcon();
+       const deletebtn = this.trashIcon();
        deletebtn.addEventListener('click', () => {
             console.log('yeeee')
        })
     }
 };
-
 
 /* create task container  */
 class ProjectContainer {
@@ -48,43 +68,25 @@ class ProjectContainer {
     }
     createProjectFolder() {
         const nav = document.getElementById('nav');
-        nav.append(taskContainerInstance.innerHTML)
-    }
-    taskItem() {
-        const taskItem = document.createElement('li');
-        taskItem.setAttribute('class', 'task-item');
-        const addTaskText = "Add Task Here";
-        taskItem.append(addTaskText)
-        this.taskContainer.append(taskItem);
-        return taskItem;
-    }
-    appendTask(taskContainer) {
-        const deletebtn = TaskManager.trashIcon();
-        taskContainer.append('hi');
-/*         deletebtn.addEventListener('click', () => {
-            taskItem.remove();
-        }) */
-        mainContent.append(taskContainer);
-        console.log(taskContainer)
-        return this
-    }
+        nav.append(taskContainer.innerHTML)
 
-    
+    }
 };
  
 
 /* initialize project */
 function init() {
-    const task = new TaskManager();
-    const taskContainerInstance = new ProjectContainer();
-    const addTaskIcon = task.addIcon();
-    header.appendChild(addTaskIcon);
-    mainContent.append('hi')
-    console.log(taskContainerInstance)
 
-/*     header.addEventListener('click', task.createNewTask);
-    task.deleteTask(); */
+    const task = new TaskManager();
+    const addTask = task.addIcon();
+    task.appendElement(taskContainer.taskContainer);
+    
+    header.appendChild(addTask);
+    header.addEventListener('click', task.createNewTask);
+    task.deleteTask();
 }
+const taskContainer = new ProjectContainer();
+const test = taskContainer.createProjectFolder();
 
 
 init();
